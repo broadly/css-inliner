3377b853413530b7ac7b581e61e8049f5756c63f
Assaf Arkin <assaf@labnotes.org>
2015-09-22 16:50:08 -0700

    Added the functional test

---
+++ test/inline_css_test.js
@@ -0,0 +1,28 @@
+'use strict';
+const assert    = require('assert');
+const CSSInline = require('../');
+const File      = require('fs');
+
+
+describe('Inline CSS', function() {
+
+  let actual;
+
+  before(function() {
+    const inliner = new CSSInline({
+      directory: __dirname
+    });
+    const source  = File.readFileSync(`${__dirname}/inline.html`, 'utf-8');
+    return inliner
+      .inlineAsync(source)
+      .then(function(result) {
+        actual = result;
+      });
+  });
+
+  it('should produced inlined document', function() {
+    const expected = File.readFileSync(`${__dirname}/inline.expected.html`, 'utf-8');
+    assert.equal(actual, expected);
+  });
+
+});
