cb7e720d2d88c29900bae8b56cf6e7eedb0d24c3
Assaf Arkin <assaf@labnotes.org>
2015-09-26 15:38:38 -0700

    Added tests/example for working with Less

---
+++ test/less_test.js
@@ -0,0 +1,75 @@
+'use strict';
+const assert        = require('assert');
+const Cache         = require('../lib/cache');
+const Context       = require('../lib/context');
+const CSSselect     = require('css-select');
+const extractRules  = require('../lib/extract_rules');
+const Less          = require('less');
+const loadFrom      = require('../lib/load_from');
+const parseHTML     = require('../lib/parse_html');
+
+
+describe('Extract Less stylesheets', function() {
+
+
+  // html -> { dom, rules }
+  function parseAndExtract(html) {
+
+    const loadAsync = loadFrom(__dirname);
+    function loadLessAsync(filename) {
+      return loadAsync(filename)
+        .then(source => source.toString())
+        .then(less => Less.render(less))
+        .then(result => result.css);
+    }
+
+    const cache   = new Cache({ loadAsync: loadLessAsync });
+    const context = new Context({ html, cache });
+    const parsed  = parseHTML(context);
+    return extractRules(parsed);
+  }
+
+
+  describe('that exists', function() {
+
+    let rules;
+    let dom;
+
+    before(function() {
+      const html = '<head><link rel="stylesheet" href="/less_test.less"></head>';
+      return parseAndExtract(html)
+        .then(function(context) {
+          dom   = context.dom;
+          rules = context.rules;
+        });
+    });
+
+    it('should remove all link elements', function() {
+      const linkElements = CSSselect.selectAll('link', dom);
+      assert.equal(linkElements.length, 0);
+    });
+
+    it('should collect all rules', function() {
+      assert.equal(rules.size, 1);
+      const rule = rules.get(0).toString().replace(/\s+/g, ' ');
+      assert.equal(rule, 'div h1 { color: blue; }');
+    });
+
+  });
+
+
+  describe('missing stylesheet', function() {
+
+    it('should error', function() {
+      const html    = `<link rel="stylesheet" href="/not_found.less">`;
+      return parseAndExtract(html)
+        .then(function() {
+          assert(false, 'Expected an error');
+        })
+        .catch(function() { });
+    });
+  });
+
+});
+
+
