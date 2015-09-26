90e2f879971fad754ea4bcd4d9b1f20dfb200750
Assaf Arkin <assaf@labnotes.org>
2015-09-26 15:23:53 -0700

    Clean up extract rules

--- test/extract_rules_test.js
+++ test/extract_rules_test.js
@@ -7,49 +7,51 @@ const extractRules  = require('../lib/extract_rules');
 const parseHTML     = require('../lib/parse_html');
 
 
-// [ Rule ] -> [ string ]
-function ruleNames(rules) {
-  return rules.map(function(rule) {
-    return rule.name ? `@${rule.name}` : rule.selector;
-  });
-}
-
-
 describe('Extract stylesheets', function() {
 
-  const cache   = new Cache({ directory: __dirname });
+  // [ rule ] -> [ string ]
+  function ruleNames(rules) {
+    return rules.map(function(rule) {
+      return rule.name ? `@${rule.name}` : rule.selector;
+    });
+  }
 
-  describe('exists in file system', function() {
+  // html -> { dom, rules }
+  function parseAndExtract(html) {
+    const cache   = new Cache({ directory: __dirname });
+    const context = new Context({ html, cache });
+    const parsed  = parseHTML(context);
+    return extractRules(parsed);
+  }
 
-    const html =
-  `<html>
-    <head>
-      <!-- extracted -->
-      <style>
-        @media screen {
-          h1 { color: red; }
-        }
-        h2 { color: green; }
-        h3, h2:hover {
-          text-decoration: underline;
-        }
-      </style>
-      <link rel="stylesheet" href="/extract_rules.css">
-      <!-- not extracted -->
-      <link rel="stylesheet" href="http://example.com/external.css">
-    </head>
-  </html>`;
+
+  describe('that exists', function() {
 
     let rules;
     let dom;
 
     before(function() {
-      const context = new Context({ html, cache });
-      const parsed  = parseHTML(context);
-      dom = parsed.dom;
-
-      return extractRules(parsed)
+      const html =
+`<html>
+  <head>
+    <!-- extracted -->
+    <style>
+      @media screen {
+        h1 { color: red; }
+      }
+      h2 { color: green; }
+      h3, h2:hover {
+        text-decoration: underline;
+      }
+    </style>
+    <link rel="stylesheet" href="/extract_rules.css">
+    <!-- not extracted -->
+    <link rel="stylesheet" href="http://example.com/external.css">
+  </head>
+</html>`;
+      return parseAndExtract(html)
         .then(function(context) {
+          dom   = context.dom;
           rules = context.rules;
         });
     });
@@ -59,19 +61,22 @@ describe('Extract stylesheets', function() {
       assert.equal(styleElements.length, 0);
     });
 
-    it('should remove only resolved relative stylesheets', function() {
-      const styleElements = CSSselect.selectAll('link[rel~="stylesheet"]', dom);
-      assert.equal(styleElements.length, 1);
-      const href = styleElements[0].attribs.href;
+    it('should remove only local stylesheets', function() {
+      const linkElements = CSSselect.selectAll('link[rel~="stylesheet"]', dom);
+      assert.equal(linkElements.length, 1);
+
+      const href = linkElements[0].attribs.href;
       assert( href.startsWith('http://') );
     });
 
     it('should collect all rules', function() {
       assert.equal(rules.size, 4);
+
+      const rule = rules.get(3).toString().replace(/\s+/g, ' ');
+      assert.equal(rule, 'body { color: blue; }');
     });
 
     it('should collect rules in document order', function() {
-
       const names = ruleNames( rules );
       assert.deepEqual(names.toArray(), [ '@media', 'h2', 'h3, h2:hover', 'body' ]);
     });
@@ -81,22 +86,11 @@ describe('Extract stylesheets', function() {
 
   describe('invalid CSS', function() {
 
-    const html =
-`<html>
-  <head>
-    <style>
-      @media screen {
-        h1 { color: red; }
-    </style>
-  </head>
-</html>`;
-
     let parseError;
 
     before(function() {
-      const context = new Context({ html, cache });
-      const parsed  = parseHTML(context);
-      return extractRules(parsed)
+      const html = '<style>@media screen { h1 { color: red; </style>';
+      return parseAndExtract(html)
         .catch(function(error) {
           parseError = error;
         });
@@ -104,11 +98,11 @@ describe('Extract stylesheets', function() {
 
 
     it('should report error with line number', function() {
-      assert.equal(parseError.line, 2);
+      assert.equal(parseError.line, 1);
     });
 
     it('should report error with column number', function() {
-      assert.equal(parseError.column, 7);
+      assert.equal(parseError.column, 17);
     });
 
     it('should report error with reason', function() {
@@ -120,26 +114,18 @@ describe('Extract stylesheets', function() {
 
   describe('missing stylesheet', function() {
 
-    const html =
-  `<html>
-    <head>
-      <link rel="stylesheet" href="/not_found">
-    </head>
-  </html>`;
-
     let loadError;
 
     before(function() {
-      const context = new Context({ html, cache });
-      const parsed  = parseHTML(context);
-      return extractRules(parsed)
+      const html    = `<link rel="stylesheet" href="/not_found.css">`;
+      return parseAndExtract(html)
         .catch(function(error) {
           loadError = error;
         });
     });
 
     it('should report error with path', function() {
-      assert.equal(loadError.path, `${__dirname}/not_found`);
+      assert.equal(loadError.path, `${__dirname}/not_found.css`);
     });
 
     it('should report error with code', function() {
