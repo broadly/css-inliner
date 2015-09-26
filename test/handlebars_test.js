65dc3f06416d3a11a737b7c2728b1ed4e5f4af7e
Assaf Arkin <assaf@labnotes.org>
2015-09-26 15:10:32 -0700

    Added failing tests for Handlebars support

---
+++ test/handlebars_test.js
@@ -0,0 +1,116 @@
+'use strict';
+const assert    = require('assert');
+const Context   = require('../lib/context');
+const domToHTML = require('../lib/dom_to_html');
+const parseHTML = require('../lib/parse_html');
+
+
+describe('Handlebar templates', function() {
+
+  function roundTrip(html) {
+    const context     = new Context({ html });
+    const parsed      = parseHTML(context);
+    const serialized  = domToHTML(parsed);
+    return serialized.html;
+  }
+
+  it('should allow expressions', function() {
+    const html      = '<h1>{{message}}</h1><input value="{{time}}">';
+    const expected  = html;
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+  it('should allow expressions (complex case)', function() {
+    const html      = '{{#each articles.[10].[#comments]}}{{/each}}';
+    const expected  = html;
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+  it('should allow un-encoded expressions', function() {
+    const html      = '<h1>{{{raw}}}</h1>';
+    const expected  = html;
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+  it('should allow helpers (with quoted args)', function() {
+    const html      = '{{{link "See more..." href=story.url class="story"}}}';
+    const expected  = html;
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+  it('should allow sub-expressions (with quoted args)', function() {
+    const html      = '{{outer-helper (inner-helper "abc") "def"}}';
+    const expected  = html;
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+  it('should allow escaped blocks', function() {
+    const html      = '\\{{escaped}}\n{{{{raw}}}{{escaped}}{{{{/raw}}}}';
+    const expected  = html;
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+  it('should allow escaped blocks (with quoted args)', function() {
+    const html      = '\\{{escaped "foo"}}\n{{{{raw "bar"}}}{{escaped "foo"}}{{{{/raw}}}}';
+    const expected  = html;
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+  it('should allow blocks around elements', function() {
+    const html      = '{{#if user}}<h1>Welcome back</h1>{{/else}}Login{{/if}}';
+    const expected  = html;
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+  it('should allow blocks in attribute values', function() {
+    const html      = '<input value="{{#if user}}{{user}}{{/else}}unknown{{/if}}">';
+    const expected  = html;
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+  it('should allow blocks in attribute values (with quoted args)', function() {
+    const html      = '<input value="{{#if user}}{{format user "f.L"}}{{/else}}unknown{{/if}}">';
+    const expected  = html;
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+  it('should allow blocks with parameters', function() {
+    const html      = '{{#each users as |user userId|}}\nId: {{userId}} Name: {{user.name}}\n{{/each}}';
+    const expected  = html;
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+  it('should allow comments', function() {
+    const html      = '{{! This comment will not be in the output }}\n<!-- This comment will be in the output -->';
+    const expected  = '{{! This comment will not be in the output }}\n';
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+  it('should allow comments (with quotes)', function() {
+    const html      = '{{! This "comment" should work }}';
+    const expected  = html;
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+  it('should allow partials (with quoted args)', function() {
+    const html      = '{{> userMessage tagName="h1" }}';
+    const expected  = html;
+    const actual    = roundTrip(html);
+    assert.equal(actual, expected);
+  });
+
+});
+
